20078f3d3d01638d2c5e621f2173ba49eac6255f
Sierra Tucker <sierra.tucker121@gmail.com>
2022-02-08 21:44:28 -0700

    Update: handle invalid input

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :handle_error_404
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_error_400
 
   def handle_error_404(error)
     render json: { errors: { exception: error.to_s} }, status: :not_found
+  end
+
+  def handle_error_400(error)
+    render json: { errors: {execption: error.to_s} }, status: 400
   end
 end
