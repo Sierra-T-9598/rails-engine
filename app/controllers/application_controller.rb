e69aebd7066556ca4e7e0f6def226a87c1962e55
Sierra Tucker <sierra.tucker121@gmail.com>
2022-02-09 17:52:42 -0700

    Update: refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound, with: :handle_error_404
-  rescue_from ActiveRecord::RecordInvalid, with: :handle_error_400
+  rescue_from ActiveRecord::RecordNotFound, with: :error_404
+  rescue_from ActiveRecord::RecordInvalid, with: :error_400
 
-  def handle_error_404(error)
+  def error_404(error)
     render json: { errors: { exception: error.to_s} }, status: :not_found
   end
 
-  def handle_error_400(error)
+  def error_400(error)
     render json: { errors: {execption: error.to_s} }, status: 400
   end
 end
