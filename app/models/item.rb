79b99ecb5bddabe9d541423f7d8eb420672b10f0
Sierra Tucker <sierra.tucker121@gmail.com>
2022-02-09 17:51:32 -0700

    Feat: create corresponding four class methods to accomplish `find_all_items` searches

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,25 @@ class Item < ApplicationRecord
   validates_presence_of(:description)
   validates_presence_of(:unit_price)
   validates :unit_price, numericality: { greater_than: 0.0 }
+  #class methods
+  def self.find_all_items_by_name(query)
+    where('name ILIKE ?', "%#{query}%")
+    .order(:name)
+  end
+
+  def self.find_all_items_filter_min(query)
+    where("unit_price >= #{query}")
+    .order(:name)
+  end
+
+  def self.find_all_items_filter_max(query)
+    where("unit_price <= #{query}")
+    .order(:name)
+  end
+
+  def self.find_all_items_between(min, max)
+    where("unit_price >= #{min}")
+    .where("unit_price <= #{max}")
+    .order(:name)
+  end
 end
